a49d41608400ee7ddf9be9ead8a9b6f416e9fbfa
Tatsuro-ai <94831442+Tatsuro-ai@users.noreply.github.com>
2021-11-29 13:07:28 +0900

    update

--- streamlittest.py
+++ streamlittest.py
@@ -6,18 +6,40 @@ Created on Mon Nov 22 17:06:14 2021
 """
 
 import streamlit as st
+import random
+kuji1 = ['まったなしだ。','待っていた。。任務完了だ','何の面白みもない。','俺にはもう時間がない。','的を射ている。']
 
-st.write('こんにちは')
+st.title('バーソロミュー・くま')
+st.write('わたしはニキュニキュの実の肉球人間 ')
+
+st.write('旅行するならどこへ行きたい？')
 
 text = st.text_input('あなたの名前を教えてください')
-'あなたの名前は,', text, 'です'
+'お前の名前は,', text, 'だな。'
 
-
-from PIL import image
-img = Image.open('IMG_E4380.JPG')
-st.image(img, caption='写真の説明',use_column_width=True)
-
+s = st.selectbox('好きな気候を選択してほしい',('-','寒帯気候','亜寒帯気候','温帯気候','熱帯気候','乾燥帯気候'))
 left_column, right_column = st.columns(2)
-button = left_column.button('右カラムに文字を表示')
+button = left_column.button('くま名台詞')
 if button:
-right_column.write('ここは右カラムです')
+          
+    right_column.write(random.choice(kuji1))
+    
+if s ==('寒帯気候'):
+    st.write('あなたは千葉県千葉市花見川区に飛ばされました')
+    st.write('理由：どちらかというと東京に近いので人間性が冷たい人が多そう')
+if s ==('亜寒帯気候'):
+    st.write ('あなたは千葉県千葉市中央区に飛ばされました')
+    st.write('理由：千葉の中心だと思いあがっていて人間性が冷たい人が多そう')
+if s ==('温帯気候'):
+    st.write ('あなたは千葉県千葉市緑区に飛ばされました')
+    st.write('理由：程よい自然で住みやすく人間性が温かい')
+if s ==('熱帯気候'):
+    st.write ('あなたは千葉県千葉市美浜区に飛ばされました')
+    st.write('理由：幕張メッセがあり年中イベントを行っているため熱気がありそう')
+if s ==('乾燥帯気候'):
+    st.write ('あなたは千葉県千葉市若葉区に飛ばされました')
+    st.write('理由：なんか乾燥してそう')
+if s==('-'):
+    st.write('選択してください')
+    
+
